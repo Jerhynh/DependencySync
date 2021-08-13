fb17c152536d2acd832cb5dd170386e141ffbee7
Jerhyn <60614943+Jerhynh@users.noreply.github.com>
2021-08-13 18:03:46 -0500

    Pushing progress due to thunderstorm

--- Src/DependencySync/Models/DSOperation.cs
+++ Src/DependencySync/Models/DSOperation.cs
@@ -3,6 +3,7 @@
     class DSOperation
     {
         public List<string> Dependencies = new();
+        public bool CopySubDirectories { get; set; } = true;
         public string DeliveryPath { get; set; } = string.Empty;
     }
 }
--- Src/DependencySync/Program.cs
+++ Src/DependencySync/Program.cs
@@ -15,6 +15,62 @@ namespace DependencySync
             DSOperation SyncOperation = ArgumentParsing.ParseArgs(args);
         }
 
-        
-    }
+        private static void SyncWorker(DSOperation SyncOperation)
+        {
+            if (SyncOperation.Dependencies.Count() == 0) return;
+            if (String.IsNullOrEmpty(SyncOperation.DeliveryPath)) return;
+
+            foreach (var dependency in SyncOperation.Dependencies)
+            {
+                if (File.Exists(dependency))
+                {
+                    File.Copy(dependency, SyncOperation.DeliveryPath);
+                }
+                else if (Directory.Exists(dependency))
+                {
+                    DirectoryCopy(dependency, SyncOperation.DeliveryPath, SyncOperation.CopySubDirectories);
+                }
+                else
+                {
+                    Console.WriteLine("Unable to sync {}");
+                }
+                
+            }
+        }
+
+        private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            // If the destination directory doesn't exist, create it.       
+            Directory.CreateDirectory(destDirName);
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string tempPath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(tempPath, false);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string tempPath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, tempPath, copySubDirs);
+                }
+            }
+        }
 }
